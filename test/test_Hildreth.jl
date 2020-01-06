731bbc8e771cbef492b23118e52fecbf81a01063
EdwardStables <edward.stables1198@gmail.com>
2020-01-06 16:40:03 +0000

    added method specific testing to test_hildreth

--- test/test_Hildreth.jl
+++ test/test_Hildreth.jl
@@ -1,5 +1,44 @@
 @testset "Hildreth" begin
     #Standard Tests
     RowActionMethodStandardTests(Hildreth())
+
+    empty_model = RowActionMethods.HildrethModel([],[],[],[],[],[],[],
+                                                 Nothing(),Nothing(),
+                                                 Dict("iterations"=>0))
+    built_model = RowActionMethods.HildrethModel(
+                  [1.0 0.0; 0.0 1.0], [-2.0, -2.0], [1.0 0.0; -1.0 0.0; 0.0 1.0; 0.0 -1.0], 
+                  [1.0, 0.0, 1.0, 0.0], 
+                  [1.0 -1.0 0.0 0.0; -1.0 1.0 0.0 0.0; 0.0 0.0 1.0 -1.0; 0.0 0.0 -1.0 1.0], 
+                  [-1.0, 2.0, - 1.0, 2.0], [2.0, 2.0], nothing, [1 0; 0 1], 
+                  Dict{String,Any}("iterations" => 0,"λ" => [0.0, 0.0, 0.0, 0.0], 
+                                   "λ_old" => [0.0, 0.0, 0.0, 0.0])
+                 )
     
+    problem = example_2()
+    question = problem["prob"]
+    
+    @testset "Model" begin
+        model = Optimizer(Hildreth()) 
+        @test model == empty_model
+        buildmodel!(model, question...)
+        @test model == built_model
+        @test empty_model != built_model
+    end
+
+    @testset "Iterations" begin
+        #TODO: Implement this with some example problems that need more iterations
+    end
+    
+    @testset "Functions" begin
+        built_model.ucSoln = [1; 1]    
+        @test RowActionMethods.valid_unconstrained(built_model) == true
+        built_model.ucSoln = [2; 2]    
+        @test RowActionMethods.valid_unconstrained(built_model) == false
+
+        RowActionMethods.set_unconstrained!(built_model)
+        @test built_model.Soln == built_model.ucSoln
+
+        @test answer(built_model) == built_model.Soln
+    end
+
 end
