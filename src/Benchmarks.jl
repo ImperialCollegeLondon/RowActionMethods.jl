4fe21fed7cfa841b2735d361a289e98bed538519
EdwardStables <edward.stables1198@gmail.com>
2020-02-17 18:21:21 +0000

    Added temporary benchmarking functions

--- src/Benchmarks.jl
+++ src/Benchmarks.jl
@@ -1,26 +1,19 @@
 using LinearAlgebra
 using BenchmarkTools
 using MathOptInterface
+import JuMP
 
-#include("ProblemList.jl")
-
-#Currently only using the problems available in OptimizationProblems repo for 
-#simplicity CUTEst naming convention is available here: 
-#http://www.cuter.rl.ac.uk//Problems/classification.shtml
-#This solver requires quadratic problems with linear constraints, therefore 
-#classification of QLR{0,1,2}-.N-dd is suitable. This gives quite small problems
-#from this problem set, larger are needed in future.
-
-function run_all_benchmarks()
-    single_benchmark(QLR2_AN_4_6)
+function single_benchmark_ram(n, m, i)
+    @benchmark JuMP.optimize!(model) setup=(model=JuMP.Model(JuMP.with_optimizer(RAM.Optimizer, "Hildreth", iterations=$i));
+                                            jump_generate_test(model, $n, $m))
 end
 
-function single_benchmark(problem)
-    prob, var = problem()
-    #Build problem build benchmark
-    @benchmark set_optimizer($prob, with_optimizer(RAM.Optimizer, "Hildreth"))
-    @benchmark optimize($prob)
-end
+#using Ipopt
+#function single_benchmark_ipopt(n, m, i)
+#    #Build problem build benchmark
+#    @benchmark JuMP.optimize!(model) setup=(model=JuMP.Model(JuMP.with_optimizer(Ipopt.Optimizer));
+#                                            jump_generate_test(model, $n, $m))
+#end
 
 """
     function generate_random_mat(n::Int, ::Type{T}; entry_gen=rand, non_zero_gen=0.1)::Array{T,2} where T
@@ -50,12 +43,21 @@ function generate_random_posdef_matrix(n::Int, ::Type{T}; entry_gen=rand, non_ze
     return M
 end
 
-function generate_random_vector(n::Int, ::Type{T}; entry_gen=rand, non_zero_gen::Float64=0.1)::Vector{T} where T
+function generate_random_vector(n::Int, ::Type{T}; 
+                                entry_gen=rand, 
+                                non_zero_gen::Float64=0.1,
+                                zero_vectors::Bool=true
+                               )::Vector{T} where T
     V = zeros(T,n)
 
-    for i=1:n
-        if rand() < non_zero_gen
-            V[i] = entry_gen(T)
+    while true
+        for i=1:n
+            if rand() < non_zero_gen
+                V[i] = entry_gen(T)
+            end
+        end
+        if zero_vectors || V != zeros(T,n)
+            break
         end
     end
 
@@ -90,7 +92,7 @@ function moi_generate_test(model::Optimizer, n::Int, m::Int
     #Constraint generation
     con = MOI.ConstraintIndex[]
     for i = 1:m
-        F = 10 .* generate_random_vector(n, Float64, non_zero_gen=0.5)
+        F = 10 .* generate_random_vector(n, Float64, non_zero_gen=0.5, zero_vectors=false)
         func = MOI.ScalarAffineFunction(
                map((c, i) -> MOI.ScalarAffineTerm(c, i), F, x), 0.0)
         lim = MOI.LessThan(rand())
@@ -100,3 +102,50 @@ function moi_generate_test(model::Optimizer, n::Int, m::Int
     return x, con
 end
 
+"""
+    jump_generate_test(model::JuMP.Model, n::Int, m::Int)
+
+Generates a random QP minimisation problem in `model`. Sets constraints within the 
+model with the name `con` and constraints with the name `x`. Access these variables 
+using the syntax:
+- con = model[:con]
+- x = model[:x]
+respectively.
+"""
+function jump_generate_test(model::JuMP.Model, n::Int, m::Int)
+    JuMP.@variable(model, x[1:n])
+
+    E = generate_random_posdef_matrix(n, Float64)
+    F = generate_random_vector(n, Float64)
+
+    JuMP.@objective(model, Min, 0.5sum(x[i]E[i,j]x[j] for i=1:n,j=1:n) + sum(x[i]F[i] for i=1:n))
+
+
+    γ = generate_random_vector(m, Float64, non_zero_gen=1.0)
+    M = zeros(m,n)
+    for i=1:m
+        M[i,:] = generate_random_vector(n, Float64, zero_vectors=false) 
+    end
+    
+    JuMP.@constraint(model, con, M*x .<= γ)
+end
+
+function jump_generate_multiple(count::Int, n::Int, m::Int; identical::Bool=false)::Vector{JuMP.Model}
+    models = JuMP.Model[]
+    
+    if identical == false
+        for i in 1:count
+            model = JuMP.Model()
+            jump_generate_test(model, n, m)
+            push!(models, model)
+        end
+    else
+        model = JuMP.Model()
+        jump_generate_test(model, n, m)
+        for i in 1:count
+            push!(models, copy(model))
+        end
+    end
+
+    return models
+end
