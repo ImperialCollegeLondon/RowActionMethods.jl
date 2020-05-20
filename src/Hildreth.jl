c988292b1e556bc6ef6742b2ce58c314cc1317c3
EdwardStables <edward.stables1198@gmail.com>
2020-05-20 15:23:18 +0100

    Refactored hildreth to be identical to literature

--- src/Hildreth.jl
+++ src/Hildreth.jl
@@ -15,16 +15,18 @@ workingvars - a Dict of values adjusted between iterations\n
 options - [currently unused] specific values used for setting solver-specific values
 """
 mutable struct Hildreth{T} <: ModelFormulation
-    H::SparseMatrixCSC{T}
-    K::SparseVector{T}
-    ucSoln::SparseVector{T}
-    Soln::SparseVector{T}
-    E_fact::SparseMatrixCSC{T}
-    λ::Vector{T}
-    λ_old::Vector{T}
+    D::SparseMatrixCSC{T}
+
+    A::SparseMatrixCSC{T}
+    b::SparseVector{T}
+
+    Δ::SparseMatrixCSC{T}
+    n::Int
+
+    z::SparseVector{T}
+    x::SparseVector{T}
 
     Hildreth{T}() where T = new()
-
 end
 
 ObjectiveType(::Hildreth) = Quadratic()
@@ -36,6 +38,37 @@ function DeleteVariable(method::Hildreth, index)
 end
 
 """
+    Build(model::Hildreth)
+
+Builds the internal variables based on problem specification
+"""
+function Build(model::RAMProblem, method::Hildreth)
+    G = get_constraintmatrix(model)'
+    h = get_constraintvector(model)
+    B, d = GetObjective(model)
+    
+    #TODO: Likely an iterative solution that avoids casting F to a dense matrix
+    #TODO re-implement E_fact
+    #method.E_fact    = RegisterSparse(model, 
+    
+    #TODO is it required to cast to matrix here?
+    method.D        = cholesky(Matrix(B)).U
+    #E_fact           = GetSparse(model, method.E_fact)
+
+    
+    method.A        = G/method.D
+    method.b        = h + (G/B)d
+
+    method.Δ        = (method.A * method.A')'
+
+    method.n        = length(h)
+
+    #TODO adjust this range depending on input value
+    method.z        = rand(0.1:0.1:10.0, method.n)
+    method.x        = -method.A'method.z
+end
+
+"""
     Iterate(model::Hildreth)
 
 Performs one iteration of the algorithm. Updates λ as it progresses.
@@ -43,66 +76,15 @@ Performs one iteration of the algorithm. Updates λ as it progresses.
 Treats the entire summation as a calculation of H_i * λ, then subtracts the 
 contribution of the currently considered λ. 
 """
-iterate_args(::Hildreth) = [:H, :K]
-function Iterate(model::RAMProblem, method::Hildreth, H, K)
-    method.λ_old = method.λ
-    for i in 1:model.constraint_count
-        w = (H[i:i,:] * method.λ)[1] - H[i,i] * method.λ[i]
-        w += K[i]
-        w /= -H[i,i]
-        method.λ[i] = max(0, w)
+iterate_args(::Hildreth) = [:z, :Δ, :b, :n]
+function Iterate(model::RAMProblem, method::Hildreth, z, Δ, b, n)
+    for i in 1:n
+        w = Δ[:,i]'*z
+        w += b[i]
+        w /= Δ[i,i]
+        w = z[i] - w
+        method.z[i] = max(0,w)
     end
-end
-
-"""
-    Build(model::Hildreth)
-
-Builds the internal variables based on problem specification
-"""
-function Build(model::RAMProblem, method::Hildreth)
-    Mt = get_constraintmatrix(model)
-    M = Mt'
-    γ = get_constraintvector(model)
-    Q, F = GetObjective(model)
-    
-    #TODO: Likely an iterative solution that avoids casting F to a dense matrix
-    F_v = Vector(F)
-    #TODO re-implement E_fact
-    #method.E_fact    = RegisterSparse(model, cholesky(E))
-    #E_fact           = GetSparse(model, method.E_fact)
-
-    
-    method.H         = M * (Q\Mt)
-    method.K         = γ + (M * (Q\F_v))
-    method.ucSoln    = -Q\F_v
-    method.Soln      = zeros(model.variable_count)
-
-    method.λ         = zeros(model.constraint_count)
-    method.λ_old     = zeros(model.constraint_count)
-end
-
-"""
-    valid_unconstrained(model::Hildreth)::Bool
-
-Returns true if the initial minimum is within the constraints.
-"""
-function valid_unconstrained(model::Hildreth)::Bool
-    valid = get_constraintmatrix(model)' * GetSparse(model.ucSoln) - get_constraintvector(model)
-    for v in valid
-        if v > 0
-            return false
-        end
-    end
-    return true
-end
-
-"""
-    set_unconstrained!(model::Hildrethmodel)
-
-Sets the model's solution element (Soln) to the unconstrained solution (ucSoln)
-"""
-function set_unconstrained!(model::Hildreth)
-    model.Soln = model.ucSoln
 end
 
 function is_empty(method::RAMProblem, model::Hildreth)
@@ -155,18 +137,13 @@ for the problem: min 1/2(x'Ex) + F'x s.t. Mx <= γ
 Where E_f is the factorised form of E in the problem statement, and λ is
 a working variable of the row action method.
 """
-resolve_args(::Hildreth) = [:λ]
-function Resolve(model::RAMProblem, method::Hildreth, λ)
-    Mt = get_constraintmatrix(model)
-    Q, F = GetObjective(model)
-    method.Soln = -(Q\Vector(F + Mt * λ))
+resolve_args(::Hildreth) = [:A, :z]
+function Resolve(model::RAMProblem, method::Hildreth, A, z)
+    method.x = -A'*z
 end
 
-GetVariables(model::RAMProblem, method::Hildreth) = method.Soln
-
-function objective_value(model::RAMProblem, method::Hildreth)
-    Q, F = GetObjective(model)
-    S = method.Soln
-    return 0.5 * (S' * Q * S) + (S' * F)
+function GetVariables(model::RAMProblem, method::Hildreth) 
+    B, d = GetObjective(model)
+    return method.D\method.x - B\Vector(d)
 end
 
