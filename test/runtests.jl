64af93e4a970eb1072e5653b0a404cd2bdb6ed2b
EdwardStables <edward.stables1198@gmail.com>
2020-07-23 16:52:50 +0100

    cleared old runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,5 @@ using RowActionMethods
 using LinearAlgebra
 using Test
 
-const RAM = RowActionMethods
 
-include("example_qp.jl")
 
-include("test_API.jl")
-#include("test_MOI_wrapper.jl")
-include("test_Hildreth.jl")
