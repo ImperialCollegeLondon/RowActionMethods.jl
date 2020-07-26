078d221cdd78edb41218888403d6fa875447fc6d
EdwardStables <edward.stables1198@gmail.com>
2020-07-26 10:09:44 +0100

    added some tests for core.jl

--- test/test_Core.jl
+++ test/test_Core.jl
@@ -13,6 +13,49 @@
         problem = RAM.GetModel("Hildreth")
         @test typeof(problem) == RAM.RAMProblem{Float64,Int64}
         @test typeof(problem.method) == RAM.Hildreth{Float64}
-
     end
+
+    @testset "ObjectiveType" begin 
+        problem = RAM.GetModel("Hildreth")
+        @test RAM.ObjectiveType(problem) == RAM.ObjectiveType(problem.method) == RAM.Quadratic()
+    end
+
+    @testset "SetThreads" begin
+        problem = RAM.GetModel("Hildreth")
+        @test problem.threads == false
+
+        RAM.SetThreads(problem)
+        @test problem.threads == true 
+
+        RAM.SetThreads(problem; threads=true)
+        @test problem.threads == true 
+
+        RAM.SetThreads(problem; threads=false)
+        @test problem.threads == false
+    end
+
+    @testset "GetObjective" begin 
+        
+    end
+
+    @testset "GetObjectiveFactorised" begin end
+
+    @testset "Iterate" begin end
+
+    @testset "Resolve" begin end
+    @testset "GetVariables" begin end
+    @testset "is_empty" begin end
+    @testset "get_model_status" begin end
+    @testset "GetObjectiveValue" begin end
+    @testset "SupportsDeleteConstraint" begin end
+    @testset "IterateRow" begin end
+    @testset "GetTempVar" begin end
+    @testset "VarUpdate" begin end
+    @testset "GetObjectiveValue" begin end
+
+    @testset "Setup" begin end
+    @testset "RunBuild" begin end
+    @testset "Optimize" begin end
+
+
 end
