5619d0b7be312bd43213e8a5e61537cb18700911
EdwardStables <edward.stables1198@gmail.com>
2020-07-27 15:27:57 +0100

    added docs template

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,23 @@
 using Documenter, RowActionMethods
 
-makedocs(sitename="RowActionMethods.jl")
+makedocs(;
+    sitename="RowActionMethods.jl",
+    modules=[RowActionMethods],
+    format=Documenter.HTML(),
+    pages=[
+        "Home" => "index.md",
+        "Manual" => [
+        ],
+        "Reference" => [
+            "Public API" => "ref/public.md",
+            "Internal" => "ref/internal.md",
+        ],
+    ],
+    repo="https://github.com/ImperialCollegeLondon/RowActionMethods.jl/blob/{commit}{path}#L{line}",
+    authors="EdwardStables <edward.stables1198@gmail.com>",
+    assets=String[],
+)
+
+deploydocs(;
+    repo="github.com/ImperialCollegeLondon/RowActionMethods.jl",
+)
